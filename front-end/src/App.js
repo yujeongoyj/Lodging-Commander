b767ea390798ac0071d1f03740365fa3f31c5453
yujeongoyj <oyj3989288@gmail.com>
2024-08-12 18:31:33 +0900

    동욱님 로그인 1차 병합

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,6 +15,7 @@ import Header from "./Header";
 function App() {
     return (
         <div className="App">
+            
             <Router>
                 <Routes>
                     <Route path="/" element={<Header />} />
