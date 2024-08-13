63a0ae6c5691dd5c27ca7d04519e048afde04535
yujeongoyj <oyj3989288@gmail.com>
2024-08-13 12:30:39 +0900

    디렉토리 따로 생성

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,32 +1,27 @@
-
 import './App.css';
-import AddressForm from "./AddressForm";
-import {BrowserRouter as Router, Route, Routes  } from "react-router-dom";
-import CategoryForm from "./CategoryForm";
-import FacilityForm from "./FacilityForm";
-import HotelForm from "./HotelForm";
-import RoomForm from "./RoomForm";
-import AddHotelSuccess from "./AddHotelSuccess";
+import AddressForm from "./addHotel/AddressForm";
+import {Route, Routes} from "react-router-dom";
+import CategoryForm from "./addHotel/CategoryForm";
+import FacilityForm from "./addHotel/FacilityForm";
+import HotelForm from "./addHotel/HotelForm";
+import RoomForm from "./addHotel/RoomForm";
+import AddHotelSuccess from "./addHotel/AddHotelSuccess";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./Header";
 
 
-
 function App() {
     return (
-        <div className="App">
-            
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Header />} />
-                    <Route path="/AddressForm" element={<AddressForm />} />
-                    <Route path="/CategoryForm" element={<CategoryForm />} />
-                    <Route path="/FacilityForm" element={<FacilityForm />} />
-                    <Route path="/HotelForm" element={<HotelForm />} />
-                    <Route path="/RoomForm" element={<RoomForm />} />
-                    <Route path="/AddHotelSuccess" element={<AddHotelSuccess />} />
-                </Routes>
-            </Router>
+        <div>
+            <Header/>
+            <Routes>
+                <Route path="/AddressForm" element={<AddressForm/>}/>
+                <Route path="/CategoryForm" element={<CategoryForm/>}/>
+                <Route path="/FacilityForm" element={<FacilityForm/>}/>
+                <Route path="/HotelForm" element={<HotelForm/>}/>
+                <Route path="/RoomForm" element={<RoomForm/>}/>
+                <Route path="/AddHotelSuccess" element={<AddHotelSuccess/>}/>
+            </Routes>
         </div>
     );
 }
